cd2857f264b662943f84eb7a4dd7b184488fc4e9
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-09-09 11:37:46 -0400

    create post route router connection

--- controllers/api/createPostRoute.js
+++ controllers/api/createPostRoute.js
@@ -1,19 +1,30 @@
 const router = require('express').Router();
 const { Post } = require('../../models');
 
+
+router.get('/', async (req, res) => {
+    try {
+      const postData = await Post.findAll();
+      res.status(200).json(postData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
+
 router.post('/', async (req, res) => {
     try {
-      const createPostData = await Post.create(req.body);
-      res.status(200).json(createPostData);
-      if (!createPostData) {
-        res
-          .status(400)
-          .json({ message: 'Must fill out post to create a post' });
-        return;
-      }
+        const createPostData = await Post.create(req.body);
+        res.status(200).json(createPostData);
+        if (!createPostData) {
+            res
+                .status(400)
+                .json({ message: 'Must fill out post to create a post' });
+            return;
+        }
     } catch (err) {
-        res.status(400).json(err); 
+        res.status(400).json(err);
     }
-    });
-  
-     
+});
+
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
+const createPostRoute = require('./createPostRoutes');
 
 router.use('/users', userRoutes);
 
