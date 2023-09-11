3c3e1e88ddf6074d49ba6764c358ec514d4780c9
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-09-11 03:31:48 -0400

    Added get :id router + previousposts

--- controllers/api/createPostRoute.js
+++ controllers/api/createPostRoute.js
@@ -15,8 +15,6 @@ router.post('/write', async (req, res) => {
     try {
         const createPostData = await Post.create(req.body);
         res.status(200).json(createPostData);
-       /* res.json({status: 'success',
-    Date: }) */
         if (!createPostData) {
             res
                 .status(400)
@@ -28,5 +26,18 @@ router.post('/write', async (req, res) => {
     }
 });
 
+router.get('/:id', async (req, res) => {
+    try {
+        const postData = await Post.findByPk(req.params.id);
+        if (postData) {
+            const post = postData.get({ plain: true });
+            res.render('post', { post });
+        } else {
+            res.status(404).json({ message: 'No post found with that id!' });
+        }
+    } catch (err) {
+        res.status(500).json(err);
+    }
+});
 
 module.exports = router;
