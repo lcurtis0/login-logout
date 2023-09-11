34a85d709b2cf56e09c9eef0a9864de2b1110d38
lcurtis0 <133440522+lcurtis0@users.noreply.github.com>
2023-09-11 12:41:58 -0400

    can post

--- controllers/api/createPostRoute.js
+++ controllers/api/createPostRoute.js
@@ -15,6 +15,7 @@ router.post('/write', async (req, res) => {
     try {
         const createPostData = await Post.create(req.body);
         res.status(200).json(createPostData);
+
         if (!createPostData) {
             res
                 .status(400)
@@ -26,18 +27,7 @@ router.post('/write', async (req, res) => {
     }
 });
 
-router.get('/:id', async (req, res) => {
-    try {
-        const postData = await Post.findByPk(req.params.id);
-        if (postData) {
-            const post = postData.get({ plain: true });
-            res.render('post', { post });
-        } else {
-            res.status(404).json({ message: 'No post found with that id!' });
-        }
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
+
+
 
 module.exports = router;
